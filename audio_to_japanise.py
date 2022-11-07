84515d0677277cdd47d86b795f636cbae1a445e3
KENTAROSZK <szkkntru2@gmail.com>
2022-11-07 21:32:59 +0900

    made catch audio, audio to japanise and real time textise files

--- audio_to_japanise.py
+++ audio_to_japanise.py
@@ -1,18 +1,114 @@
-import speech_recognition as sr
 import os
+import pickle
+import pandas as pd
+from datetime import datetime
+
+import whisper
 
 
-OUTPUT_PATH   = "./output.wav"
+# インプットファイルや出力ファイルを定義
+WAV_FILE = "./output.wav" # 文字起こし&要約したい音声ファイル
+PKL_FILE = "./textise.pkl"
+OUTPUT_TXT_FILE          = "./" + datetime.now().strftime('%Y%m%d_%H_%M') +".txt" # テキストファイルのファイル名を日付のtxtファイルにする
+OUTPUT_ONE_SENTENCE_FILE = "./one_sentence.txt"
+OUTPUT_SUMMARISED_FILE   = "./summarised.txt"
 
 
-r = sr.Recognizer()
+
+def whisper_to_textise_audio():
+    """
+    whisper ライブラリを使って、音声データをテキストにする
+    その結果をresult変数という形で圧縮保存しておく
+    """
+    if os.path.exists(PKL_FILE):
+        pass
+    else:
+        # tiny < base < small < medium < largeの順に高精度になるが、重くなる。(メモリ不足になる可能性がある)
+        model  = whisper.load_model("base")
+        result = model.transcribe(WAV_FILE, fp16=False, language="ja", verbose=True)
+        
+        with open(PKL_FILE, mode='wb') as f:
+            pickle.dump(result, f)
 
 
-with sr.AudioFile(OUTPUT_PATH) as source:
-    audio = r.record(source)
+def text_and_save():
+    """
+    whisperの結果ファイルpklをテキストファイル化して保存する
+    今回は、文字起こしした結果の生データとその生データを一つの文字列化したものの2つをtxtファイル形式で保存する
 
-print(audio)
-text = r.recognize_google(audio, language='ja-JP')
+    """
+
+    with open(PKL_FILE, mode='rb') as f:
+        result = pickle.load(f)
+
+    # whisperのモデルが区切り文字（"。"や"!"など）を作らない場合があるので
+    # 区切り文字を強制的に作成する必要がある。
+    # そのためにpandasのDataFrameにいったん格納する。
+    df_result     = pd.DataFrame(result["segments"])[["text"]]
+    series_result = df_result['text']
+
+    # 重複した文章が大量生成されている場合があるので、重複する要素があったら自動で重複排除させる
+    list_result = series_result.values.tolist()
+    list_result = list(set(list_result))
+
+    # 区切り文字を追加してリストを一つの文字列strに直す。ここでは"。"を区切り文字とする。
+    one_sentence = '。'.join(list_result)
+    
+    # 文字起こしの生データをtxtファイルとして保存
+    df_result.to_csv(OUTPUT_TXT_FILE, header=False, index=False)
+
+    # 一つの文字列化したものをtxtファイルとして保存しておく
+    with open(OUTPUT_ONE_SENTENCE_FILE, 'w', encoding='utf-8', errors='ignore') as f: #txtファイルの新規作成
+        f.write(one_sentence)
 
 
-print(text)
+def summarise(N_output = 3):
+    """
+    一つの文字列化したものを読み込んでそれを要約する。
+    """
+    from pysummarization.nlpbase.auto_abstractor import AutoAbstractor
+    from pysummarization.tokenizabledoc.mecab_tokenizer import MeCabTokenizer
+    from pysummarization.abstractabledoc.top_n_rank_abstractor import TopNRankAbstractor
+
+    # "。"を区切り文字として一つの文章にしたファイルを読み込む。
+    with open(OUTPUT_ONE_SENTENCE_FILE, encoding='utf-8', errors='ignore') as f:
+        document = f.read()
+
+    # 自動要約のオブジェクトを生成
+    auto_abstractor = AutoAbstractor()
+
+    # トークナイザーにMeCabを指定
+    auto_abstractor.tokenizable_doc = MeCabTokenizer()
+
+    # 文書の区切り文字を指定 <--これを使うために"。"を付与する処理をしていた
+    auto_abstractor.delimiter_list = ["。", "\n"]
+
+    # ドキュメントの抽象化、フィルタリングを行うオブジェクトを生成
+    abstractable_doc = TopNRankAbstractor()
+
+    # 文書の要約を実行
+    result_dict = auto_abstractor.summarize(document, abstractable_doc)
+
+    # 要約した文章は複数行になっているので、アウトプットする行数だけを保存するように変更する
+    list_temp = result_dict["summarize_result"]
+    if len(list_temp) > N_output:
+        list_temp = list_temp[:N_output]
+    
+    list_output = []
+    for line in list_temp:
+        #list_output.append(repr(line.rstrip("\n")))
+        list_output.append(line)
+
+    with open(OUTPUT_SUMMARISED_FILE, 'w') as f:
+        f.writelines(list_output)
+
+
+def main():
+    whisper_to_textise_audio()
+    text_and_save()
+    summarise()
+
+
+if __name__ == '__main__':
+    main()
+
--- catch_audio.py
+++ catch_audio.py
@@ -1,38 +1,27 @@
-import pyaudio
-import numpy as np
-from matplotlib import pyplot as plt
-from scipy.io.wavfile import write
-import speech_recognition as sr
-
-import sys
+from datetime import datetime
 import wave
 import time
-from datetime import datetime
 
-import pandas as pd
-
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout, QLabel
+import pyaudio
 
 
 FORMAT        = pyaudio.paInt16
-TIME          = 60           # 計測時間[s]
+TIME          = 10           # 録音時間[s]
 SAMPLE_RATE   = 44100        # サンプリングレート
 FRAME_SIZE    = 1024         # フレームサイズ
 CHANNELS      = 1            # モノラルかバイラルか
 INPUT_DEVICE_INDEX = 0       # マイクのチャンネル
 NUM_OF_LOOP   = int(SAMPLE_RATE / FRAME_SIZE * TIME)
-CALL_BACK_FREQUENCY = 3      # コールバック呼び出しの周期[sec]
-
-
-OUTPUT_WAV_FILE = "./output.wav"
-OUTPUT_TXT_FILE = "./" + datetime.now().strftime('%Y%m%d_%H_%M') +".txt" # テキストファイルのファイル名を日付のtxtファイルにする
-OUTPUT_ONE_SENTENCE_FILE = "./one_sentence.txt"
 
 
 
-# -------------録音機器のインデックスを探す-------------------
+WAV_FILE = "./output.wav"
+
 
 def look_for_audio_input():
+    """
+    デバイス上でのオーディオ系の機器情報を表示する
+    """
     pa = pyaudio.PyAudio()
     for i in range(pa.get_device_count()):
         print(pa.get_device_info_by_index(i))
@@ -40,10 +29,10 @@ def look_for_audio_input():
     pa.terminate()
 
 
-
-
-# -----------録音 ------------------------
 def record_and_save():
+    """
+    デバイスから出力される音声の録音をする
+    """
     pa = pyaudio.PyAudio()
 
     stream = pa.open(format   = FORMAT,
@@ -70,7 +59,7 @@ def record_and_save():
     pa.terminate()
 
 
-    wf = wave.open(OUTPUT_WAV_FILE, 'wb')
+    wf = wave.open(WAV_FILE, 'wb')
     wf.setnchannels(CHANNELS)
     wf.setsampwidth(pa.get_sample_size(FORMAT))
     wf.setframerate(SAMPLE_RATE)
@@ -78,19 +67,18 @@ def record_and_save():
     wf.close()
 
 
-# -----------再生 ------------------------
 def play_wav_file():
+    """
+    音声ファイルの再生
+    """
 
-
-    wf = wave.open(OUTPUT_WAV_FILE, 'rb')
+    wf = wave.open(WAV_FILE, 'rb')
     pa = pyaudio.PyAudio()
-
 
     stream = pa.open(format   = pa.get_format_from_width(wf.getsampwidth()),
                      channels = wf.getnchannels(),
                      rate     = wf.getframerate(),
                      output   = True)
-
 
     # ファイルの読み込み
     print("Read a file")
@@ -101,161 +89,20 @@ def play_wav_file():
     is_to_go = True
     while is_to_go:
         stream.write(data)
-        print(len(data), is_to_go)
         data = wf.readframes(FRAME_SIZE)
-        is_to_go = len(data) != 0
+        is_to_go = len(data) != 0 # "data"がゼロになったらフラグをFalseにしてwhileループを抜ける
 
     stream.close()
     pa.terminate()
 
-    print("DONE")
-
-
-
-def callback(in_data, frame_count, time_info, status):
-    global sprec 
-    try:
-        audiodata  = sr.AudioData(in_data, SAMPLE_RATE, 2)
-        sprec_text = sprec.recognize_google(audiodata, language='ja-JP')
-        
-        with open(OUTPUT_TXT_FILE,'a') as f: #ファイルの末尾に追記していく
-            f.write("\n" + sprec_text)
-    
-    except sr.UnknownValueError:
-        pass
-    
-    except sr.RequestError as e:
-        pass
-    
-    finally:
-        return (None, pyaudio.paContinue)
-
-
-def realtime_textise():
-    with open(OUTPUT_TXT_FILE, 'w') as f: #txtファイルの新規作成
-        DATE = datetime.now().strftime('%Y%m%d_%H:%M:%S')
-        f.write("日時 : " + DATE + "\n") #最初の一行目に日時を記載する
-    
-
-    global sprec 
-    
-    sprec = sr.Recognizer() # speech recogniserインスタンスを生成
-    
-    # Audio インスタンス取得
-    audio  = pyaudio.PyAudio() 
-    
-    # ストリームオブジェクトを作成
-    stream = audio.open(format             = FORMAT,
-                        rate               = SAMPLE_RATE,
-                        channels           = CHANNELS,
-                        input_device_index = INPUT_DEVICE_INDEX,
-                        input              = True, 
-                        frames_per_buffer  = SAMPLE_RATE*CALL_BACK_FREQUENCY, # CALL_BACK_FREQUENCY 秒周期でコールバック
-                        stream_callback    = callback)
-    
-    stream.start_stream()
-    
-    while stream.is_active():
-        print(datetime.now().strftime('%Y%m%d_%H_%M'))
-        time.sleep(0.001)
-    
-    stream.stop_stream()
-    stream.close()
-    audio.terminate()
-
-
-def whisper_to_textise():
-    import whisper
-
-    # tiny < base < small < medium < largeの順に高精度になるが、重くなる。(メモリ不足になる可能性がある)
-    model  = whisper.load_model("base")
-    result = model.transcribe(OUTPUT_WAV_FILE, fp16=False, language="ja")
-
-    # whisperのモデルを"base"でインスタンス化すると区切り文字"。"や"!"がテキスト化されないので、
-    # 区切り文字を強制作成するためにpandasのDataFrameにいったん格納する。
-    # 区切り文字が必要な理由はpysummarizationで区切り文字でトークナイズする必要があるため、前処理として区切り文字を付けておく必要がある
-    df_result = pd.DataFrame(result["segments"])[["text"]]
-    series_result = df_result['text']
-
-    list_result = series_result.values.tolist()
-
-    one_sentence = '。'.join(list_result)
-
-    df_result.to_csv(OUTPUT_TXT_FILE, header=False, index=False)
-
-    with open(OUTPUT_ONE_SENTENCE_FILE, 'w',encoding='utf-8', errors='ignore') as f: #txtファイルの新規作成
-        f.write(one_sentence)
-
-
-
 
 def main():
-    #look_for_audio_input() # デバイス探し
-    #record_and_save()       # 録音する場合
-    #realtime_textise()     # リアルタイム文字起こし
-
-    """
-    app    = QApplication(sys.argv)
-    widget = main_widget()
-    sys.exit(app.exec_())
-    """
-
-    print("whisper")
-    whisper_to_textise()    # whisperを使って、文字起こし
-
-
-
-
-
-
-
-
-class main_widget(QWidget):
-
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        self.resize(600, 400) # widget size : width x height
-        self.move(300, 300)
-        self.setWindowTitle('speech textise')
-        self.show()
-
-        # buttonの設定
-        self.button_start_recording  = QPushButton('start to record')
-        self.button_finish_recording = QPushButton('finish recording')
-
-
-        # クリックされたらbuttonClickedの呼び出し
-        self.button_start_recording.clicked.connect( self.button_start_recording_clicked)
-        self.button_finish_recording.clicked.connect(self.button_finish_recording_clicked)
-
-
-
-        # レイアウト配置
-        self.grid = QGridLayout()
-        self.grid.addWidget(self.button_start_recording, 0, 0, 1, 1)
-        self.grid.addWidget(self.button_finish_recording, 1, 0, 1, 2)
-        self.setLayout(self.grid)
-
-
-    def button_start_recording_clicked(self):
-        record_and_save()
-
-    def button_finish_recording_clicked(self):
-        record_and_save()
-
-
-
-
-
+    look_for_audio_input() # デバイス探し
+    record_and_save()      # デバイスから出力される音声を録音する
+    time.sleep(3)
+    play_wav_file()        # 録音した音声ファイルを再生する
 
 
 if __name__ == '__main__':
     main()
-
-
-
-
 
