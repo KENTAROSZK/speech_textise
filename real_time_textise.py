e207ff83e0adf84f7ae5fb9bece9697b734a5152
KENTAROSZK <szkkntru2@gmail.com>
2023-02-07 22:27:16 +0900

    delete unnecesarry indent

--- real_time_textise.py
+++ real_time_textise.py
@@ -32,7 +32,7 @@ def callback(in_data, frame_count, time_info, status):
     """
     コールバック関数の定義
     """
-    
+
     global sprec # speech_recognitionオブジェクトを毎回作成するのではなく、使いまわすために、グローバル変数で定義しておく
 
     try:
@@ -40,16 +40,16 @@ def callback(in_data, frame_count, time_info, status):
         sprec_text = sprec.recognize_google(audiodata, language='ja-JP')
 
         print(sprec_text)
-        
+
         with open(OUTPUT_TXT_FILE,'a') as f: #ファイルの末尾に追記していく
             f.write("\n" + sprec_text)
-    
+
     except sr.UnknownValueError:
         pass
-    
+
     except sr.RequestError as e:
         pass
-    
+
     finally:
         return (None, pyaudio.paContinue)
 
@@ -64,13 +64,13 @@ def realtime_textise():
         f.write("日時 : " + DATE + "\n") # 最初の一行目に日時を記載する
 
     global sprec # speech_recognitionオブジェクトを毎回作成するのではなく、使いまわすために、グローバル変数で定義しておく
-    
+
     # speech recogniserインスタンスを生成
     sprec = sr.Recognizer() 
-    
+
     # Audio インスタンス取得
     audio  = pyaudio.PyAudio() 
-    
+
     # ストリームオブジェクトを作成
     stream = audio.open(format             = FORMAT,
                         rate               = SAMPLE_RATE,
@@ -79,12 +79,12 @@ def realtime_textise():
                         input              = True, 
                         frames_per_buffer  = SAMPLE_RATE*CALL_BACK_FREQUENCY, # CALL_BACK_FREQUENCY 秒周期でコールバック
                         stream_callback    = callback)
-    
+
     stream.start_stream()
-    
+
     while stream.is_active():
         time.sleep(0.1)
-    
+
     stream.stop_stream()
     stream.close()
     audio.terminate()
